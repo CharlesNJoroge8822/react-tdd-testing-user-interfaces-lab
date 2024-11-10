1a63984d858da37616782ca6c9d1adf84b0f9b53
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-10 17:24:06 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+
+    <h1>Hi, I'm Charles Njoroge</h1>
+    <img className="myImage" alt="Profile image" src="/IMG-20241106-WA0035.jpg"/>
+
+    <h2>About Me</h2>
+    <p>Presently pursuing Software Engineering at Moringa School.</p>
+
+    <nav>
+
+      <a href="https://github.com/CharlesNJoroge8822">Github Link</a>
+
+
+
+      </nav> 
+  </div>;
 }
 
 export default App;
+
+
